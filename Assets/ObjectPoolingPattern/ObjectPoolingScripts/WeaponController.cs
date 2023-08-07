18b6c3baac6eed5df38920d2455e87a415a89c97
Akın Haydaroğlu <gr4ndsmurf@gmail.com>
2023-08-08 00:01:58 +0300

    Object Pooling Pattern Commit

---
+++ Assets/ObjectPoolingPattern/ObjectPoolingScripts/WeaponController.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour
+{
+    private const int POOL_SIZE = 25;
+
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private Transform firePointTransform;
+    [SerializeField] private float bulletSpeed;
+
+    private Queue<GameObject> bulletPool;
+
+    private void Start()
+    {
+        bulletPool = new Queue<GameObject>();
+        for (int i = 0; i < POOL_SIZE; i++)
+        {
+            GameObject bullet = Instantiate(bulletPrefab, Vector3.zero, Quaternion.identity);
+            bullet.SetActive(false);
+            bulletPool.Enqueue(bullet);
+        }
+    }
+    private void Update()
+    {
+        HandleAiming();
+        HandleShooting();
+    }
+
+    private void HandleAiming()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        Vector3 direction = (mouseWorldPosition - transform.position).normalized;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+
+    private void HandleShooting()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePosition = Input.mousePosition;
+            Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+            Vector3 shootDirection = (mouseWorldPosition - transform.position).normalized;
+
+            GameObject bullet = GetBulletFromPool();
+
+            if (bullet != null)
+            {
+                //Pool boþ deðilse
+                bullet.transform.position = firePointTransform.position;
+                bullet.transform.rotation = firePointTransform.rotation;
+                bullet.SetActive(true);
+
+                Rigidbody2D bulletRigidbody = bullet.GetComponent<Rigidbody2D>();
+                bulletRigidbody.velocity = new Vector2(shootDirection.x, shootDirection.y).normalized * bulletSpeed * Time.deltaTime;
+
+                StartCoroutine(DisableBulletAfterDelay(bullet, 2f));
+            }
+        }
+    }
+
+    private IEnumerator DisableBulletAfterDelay(GameObject bullet, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        ReturnBulletToPool(bullet);
+    }
+
+    private GameObject GetBulletFromPool()
+    {
+        if (bulletPool.Count > 0)
+        {
+            GameObject bullet = bulletPool.Dequeue();
+            bullet.SetActive(true);
+            return bullet;
+        }
+
+        return null;
+    }
+
+    private void ReturnBulletToPool(GameObject bullet)
+    {
+        bullet.SetActive(false);
+        bulletPool.Enqueue(bullet);
+    }
+}
